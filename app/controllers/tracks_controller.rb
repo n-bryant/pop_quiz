934cd9c6beea224f0a86e5c861270c9ea18759c8
Brentice Bush <btbush@gmail.com>
2017-02-28 08:06:37 -0500

    add endpoint to find track by id

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -4,4 +4,10 @@ class TracksController < ApplicationController
 
     render json: tracks
   end
+
+  def show
+    tracks = Track.find(params[:id])
+
+    render json: tracks
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :tracks, only: [:index, :create]
+  resources :tracks, only: [:index, :show]
 end
