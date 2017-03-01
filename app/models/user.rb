052ca4e81b160f02a01633d9e0c3c0f958d59a34
Brentice Bush <btbush@gmail.com>
2017-03-01 14:12:16 -0500

    fix user login bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
@@ -12,20 +12,20 @@ class User < ActiveRecord::Base
     end
   end
 
-  def login=(login)
-    @login = login
-  end
+  # def login=(login)
+  #   @login = login
+  # end
+  #
+  # def login
+  #   @login || self.username || self.email || self.image
+  # end
 
-  def login
-    @login || self.username || self.email || self.image
-  end
-
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_h).first
-    end
-  end
+  # def self.find_for_database_authentication(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+  #   elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+  #     where(conditions.to_h).first
+  #   end
+  # end
 end
