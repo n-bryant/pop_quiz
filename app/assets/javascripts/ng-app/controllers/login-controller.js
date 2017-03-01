7b2c09bf95799c6ff06ec1ec910127de8fd01aa1
NICHOLAS BRYANT <nbryant.iyd@gmail.com>
2017-03-01 13:31:17 -0500

    started building out login controller functionality

--- app/assets/javascripts/ng-app/controllers/login-controller.js
+++ app/assets/javascripts/ng-app/controllers/login-controller.js
@@ -1,7 +1,14 @@
 (function(ng) {
   "use strict";
 
-  ng.module('GameApp').controller('LoginController', function() {
-    
+  ng.module('GameApp').controller('LoginController', function(UserService) {
+    this.authenticate = function(username, password) {
+      const user = {
+        username: username,
+        password: password
+      };
+      console.log(user);
+      UserService.validate(user);
+    };
   });
 })(angular);
--- app/assets/javascripts/ng-app/services/user-service.js
+++ app/assets/javascripts/ng-app/services/user-service.js
@@ -1,7 +1,25 @@
 (function(ng) {
   "use strict";
 
-  ng.module('GameApp').service('UserService', function() {
-    
+  ng.module('GameApp').service('UserService', function($q, $state) {
+    this.activeUser;
+
+    function validateUser(user) {
+      console.log(user);
+      // if request is good
+        // this.activeUser = get user by user.id
+
+      // else
+        // present validation message to user
+    }
+
+    function getUser() {
+      // make data request for user information
+      // return user data that matches argument
+    }
+
+    return {
+      validate: validateUser
+    };
   });
 })(angular);
