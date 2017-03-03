42383b7303c2996bf44e4761ca0f542424858dc3
Nicholas Bryant <nicholas@Nicholass-MacBook-Pro.local>
2017-03-03 13:19:47 -0500

    built time tracking, score calculation; adjusted various styles

--- app/assets/javascripts/ng-app/controllers/gameplay-controller.js
+++ app/assets/javascripts/ng-app/controllers/gameplay-controller.js
@@ -1,90 +1,113 @@
 (function(ng) {
   "use strict";
 
-  ng.module('GameApp').controller('GameplayController', ['$q', 'DataService', '$scope', function($q, DataService, $scope) {
-    $scope.allTracks = [];
-    $scope.userGuess = '';
-    $scope.track="";
-    $scope.artist="";
-    $scope.correctguesses = 0;
-    $scope.currentTrack = null;
-    $scope.incorrectguesses = 0;
-    $scope.index = 0;
+  ng.module('GameApp').controller('GameplayController', ['$q', 'DataService', '$scope', 'UserService', '$state', function($q, DataService, $scope, UserService, $state) {
+    $scope.session = UserService.sessionStatus();
+    if (!$scope.session) {
+      $state.go('GameParent.login');
+    } else {
+      $scope.user = UserService.getActiveUser();
 
-    let index = 0;
-    //create shuffle function
-    const shuffle = function(array) {
-      let newArray = [];
-      for (let i = 0; i < array.length; i++) {
-        let j = Math.floor(Math.random() * (array.length));
-        newArray.push(array[j]);
-        array.splice(j, 1);
+      $scope.allTracks = [];
+      $scope.userGuess = '';
+      $scope.track="";
+      $scope.artist="";
+      $scope.correctguesses = 0;
+      $scope.incorrectguesses = 0;
+      $scope.score = 0;
+      $scope.timeRemaining = 30;
+
+      const guessForm = document.querySelector('.guess-form');
+
+      let index = 0;
+      let currentTrack = null;
+
+      function shuffle(array) {
+        let newArray = [];
+        for (let i = 0; i < array.length; i++) {
+          let j = Math.floor(Math.random() * (array.length));
+          newArray.push(array[j]);
+          array.splice(j, 1);
+        }
+          console.log(newArray);
+          $scope.allTracks = newArray;
+          playNext();
       }
-        console.log(newArray);
-        $scope.allTracks = newArray;
 
-        playNext();
+      $scope.guess = function() {
+          $scope.userGuess= event.target[0].value;
+          compare($scope.artist, $scope.userGuess);
+      };
+
+    function calculateScore(guessStatus) {
+      if (guessStatus) {
+        $scope.score += (2 * $scope.timeRemaining);
+        console.log($scope.score);
+      } else {
+        $scope.score -= 30;
+        if ($scope.score < 0) {
+          $scope.score = 0;
+        }
+        console.log($scope.score);
+      }
     }
 
-    $scope.guess = function() {
-        $scope.userGuess= event.target[0].value;
-        compare($scope.artist, $scope.userGuess);
+    function compare(arg1, arg2) {
+      console.log(arg1, "and",  arg2);
+      currentTrack.pause();
+      guessForm.reset();
+      arg1 = arg1.toLowerCase();
+      arg2 = arg2.toLowerCase();
+      if (arg1 === arg2) {
+        console.log('win');
+        $scope.correctguesses++;
+        index++;
+        calculateScore(true);
+      } else {
+        $scope.incorrectguesses++;
+        index++;
+        calculateScore(false);
+        console.log('lose');
+      }
+
+      if (index === $scope.allTracks.length) {
+        // send to leaderboard
+        console.log('game-over');
+      } else {
+        playNext();
+      }
     };
 
-  const calculateScore = function() {
-    console.log('score count');
-  }
+      function playNext() {
+        if (currentTrack) {
+          $scope.artist = $scope.allTracks[index].artist;
+          $scope.track = $scope.allTracks[index].preview_url;
 
-  const compare = function(arg1, arg2) {
-    console.log(arg1, "and",  arg2);
-    $scope.currentTrack.pause();
-    arg1 = arg1.toLowerCase();
-    arg2 = arg2.toLowerCase();
-    if (arg1 === arg2) {
-      console.log('win');
-      $scope.correctguesses++;
-      $scope.index++;
-    } else {
-      $scope.incorrectguesses;
-      $scope.index++;
-      console.log('lose');
-    }
+          currentTrack = new Audio($scope.track);
+          currentTrack.play();
+        } else {
+          $scope.artist = $scope.allTracks[index].artist;
+          $scope.track = $scope.allTracks[index].preview_url;
 
-    if ($scope.index === $scope.allTracks.length) {
-      calculateScore();
-      // send to leaderboard
-      console.log('game-over');
-    } else {
-      playNext();
-    }
-  };
-
-  // KEEP TRACK OF TIME AND RUN A LISTENER WITH SET INTERVAL TO WATCH FOR TIME END
-  // OR NO TIME LEFT
-
-    const playNext = function() {
-      if ($scope.currentTrack) {
-        console.log('in');
-        $scope.artist = $scope.allTracks[$scope.index].artist;
-        $scope.track = $scope.allTracks[$scope.index].preview_url;
-        $scope.currentTrack = new Audio($scope.track);
-        $scope.currentTrack.play();
-      } else {
-        $scope.artist = $scope.allTracks[$scope.index].artist;
-        $scope.track = $scope.allTracks[$scope.index].preview_url;
-
-        $scope.currentTrack = new Audio($scope.track);
-        $scope.currentTrack.play();
+          currentTrack = new Audio($scope.track);
+          currentTrack.play();
+        }
       }
+
+      function trackTime() {
+        $scope.$apply(() => {$scope.timeRemaining--});
+        console.log($scope.timeRemaining);
+        if ($scope.timeRemaining === 0) {
+          // reset animation and start next song
+        }
+      }
+      window.setInterval(function(){trackTime()}, 1000);
+
+      //retrieve allTracks from DataService
+      $q.when(DataService.getTracks()).then((response) => {
+        $scope.allTracks = response.data;
+        shuffle($scope.allTracks);
+      });
     }
-
-    //retrieve allTracks from DataService
-    $q.when(DataService.getTracks()).then((response) => {
-      $scope.allTracks = response.data;
-      console.log(response);
-      shuffle($scope.allTracks);
-      // play song 0 every time? or MathFloor(MathRandom again)?
-    });
-
   }]);
 })(angular);
