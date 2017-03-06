02fdf5701fb83554d76a050e07c9fd298c738070
Brentice Bush <btbush@gmail.com>
2017-03-06 08:32:10 -0500

    add categories to tracks and seed multiple categories of tracks from spotify

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303190031) do
+ActiveRecord::Schema.define(version: 20170306124630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20170303190031) do
     t.datetime "updated_at",  null: false
     t.string   "album_image"
     t.string   "album_name"
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_tracks_on_category_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -76,4 +78,5 @@ ActiveRecord::Schema.define(version: 20170303190031) do
 
   add_foreign_key "games", "categories"
   add_foreign_key "games", "users"
+  add_foreign_key "tracks", "categories"
 end
